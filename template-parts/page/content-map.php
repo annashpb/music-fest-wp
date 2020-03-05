50bec88d130a499f16273d167925b0c86e2bc8ec
annashpb <anna.sh.pb@gmail.com>
2020-03-05 11:33:18 +0200

    map section done

---
+++ template-parts/page/content-map.php
@@ -0,0 +1,11 @@
+<section class="map" id="map">
+    <div class="map__container">
+        <h2 class="map__headline section-headline">o show</h2>
+        <p class="map__description">
+            <?php echo get_field('map-descr'); ?>
+        </p>
+        <p class="map__text">
+            <?php echo get_field('map-text'); ?>
+        </p>
+    </div>
+</section>
